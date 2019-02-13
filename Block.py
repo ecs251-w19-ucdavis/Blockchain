66c1dd16fc394de4190a69c9cc42bab22920066c
Yue Zhang <zhangops@gmail.com>
2019-02-13 14:12:12 -0800

    new update

--- Block.py
+++ Block.py
@@ -1,6 +1,10 @@
 class head:
     def __init__(self, data):
-        self.data =  
+        self.data = data
+        self.version = 0.1
+    def versionUpdate(versionNum)
+        self.version = 0.1
+    
 class body:
 
 class block:
