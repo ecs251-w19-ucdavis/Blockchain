0b4363bc0595006349c431b7f0f075f99ddc9263
Chuan He <hecbuaa@gmail.com>
2019-02-15 16:07:39 -0800

    adding node.py

---
+++ Node.py
@@ -0,0 +1,14 @@
+import os
+import datetime
+import transactions
+
+class node:
+    neigthbor = []
+    address = ''
+    def mining(transactions):
+        
+    def transfer(to_address, amount):
+        trans = transaction(self.address, to_address, amount)
+        return trans
+    def signTransactions():
+        return
