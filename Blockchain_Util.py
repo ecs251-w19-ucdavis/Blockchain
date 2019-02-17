1bc579679b75498868ad54f9807a5206a76d81c9
Yue Zhang <zhangops@gmail.com>
2019-02-16 18:34:39 -0800

    adding static method into util calss

--- Blockchain_Util.py
+++ Blockchain_Util.py
@@ -3,18 +3,17 @@ import time
 import datetime
 
 class blockchain:
-    def __init__(self):
-        self.mining_difficulty = 2
-        self.mining_reward = 50
-        self.chain_creation_date = "02/10/2019"
-    
+    mining_difficulty = 2
+    mining_reward = 50
+    chain_creation_date = "02/10/2019"
+    @staticmethod
     def create_genesis_block(self):
         creation_date = time.mktime(datetime.datetime.strptime(self.chain_creation_date, "%d/%m/%Y").timetuple())
         return block(self.mining_difficulty, creation_date, [], 'this is the first block', 0)
-    
+    @staticmethod
     def set_mining_reward(self, len):
         if len % 2100 == 0:
             self.mining_reward /= 2
-    
+    @staticmethod
     def set_mining_difficulty(self, new_difficulty):
-        self.mining_difficulty = 2
+        self.mining_difficulty = new_difficulty
