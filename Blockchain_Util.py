6775e014968d6766ad149c11a5f0d95923438c13
Yue Zhang <zhangops@gmail.com>
2019-02-16 18:38:50 -0800

    fix argument issuse

--- Blockchain_Util.py
+++ Blockchain_Util.py
@@ -7,9 +7,9 @@ class blockchain:
     mining_reward = 50
     chain_creation_date = "02/10/2019"
     @staticmethod
-    def create_genesis_block(self):
-        creation_date = time.mktime(datetime.datetime.strptime(self.chain_creation_date, "%d/%m/%Y").timetuple())
-        return block(self.mining_difficulty, creation_date, [], 'this is the first block', 0)
+    def create_genesis_block():
+        creation_date = time.mktime(datetime.datetime.strptime(blockchain.chain_creation_date, "%d/%m/%Y").timetuple())
+        return block(blockchain.mining_difficulty, creation_date, [], 'this is the first block', 0)
     @staticmethod
     def set_mining_reward(self, len):
         if len % 2100 == 0:
