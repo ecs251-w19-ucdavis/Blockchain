6b1e5c3774663838b29a2c84dabbec2ab935f3f3
Yue Zhang <zhangops@gmail.com>
2019-02-16 17:59:04 -0800

    fix the chain creation date

--- Blockchain_Util.py
+++ Blockchain_Util.py
@@ -1,13 +1,16 @@
 from Block import block
 import time
+import datetime
 
 class blockchain:
     def __init__(self):
         self.mining_difficulty = 2
         self.mining_reward = 50
+        self.chain_creation_date = "02/10/2019"
     
     def create_genesis_block(self):
-        return block(self.mining_difficulty, time.time(), [], 'this is the first block', 0)
+        creation_date = time.mktime(datetime.datetime.strptime(self.chain_creation_date, "%d/%m/%Y").timetuple())
+        return block(self.mining_difficulty, creation_date, [], 'this is the first block', 0)
     
     def set_mining_reward(self, len):
         if len % 2100 == 0:
