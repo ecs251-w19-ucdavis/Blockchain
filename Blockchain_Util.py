66017541e8561a551e022a03669d20d1be3c6d59
WinterIsSunny <yusu@ucdavis.edu>
2019-02-20 22:42:17 -0800

    update util

--- Blockchain_Util.py
+++ Blockchain_Util.py
@@ -2,25 +2,45 @@ from Block import block
 import time
 import datetime
 
-class utils:
-    mining_difficulty = 23
+class blockchain:
+    mining_difficulty = 2
     mining_reward = 50
     chain_creation_date = "02/10/2019"
     @staticmethod
     def create_genesis_block():
-        creation_date = time.mktime(datetime.datetime.strptime(utils.chain_creation_date, "%d/%m/%Y").timetuple())
-        print(creation_date)
-        return block(utils.mining_difficulty, creation_date, [], 'this is the first block', 0)
-        
+        creation_date = time.mktime(datetime.datetime.strptime(blockchain.chain_creation_date, "%d/%m/%Y").timetuple())
+        return block(blockchain.mining_difficulty, creation_date, [], 'this is the first block', 0)
     @staticmethod
     def set_mining_reward(self, len):
         if len % 2100 == 0:
             self.mining_reward /= 2
-
     @staticmethod
     def set_mining_difficulty(self, new_difficulty):
         self.mining_difficulty = new_difficulty
 
-    @staticmethod
-    def get_difficulty():
-        return utils.mining_difficulty
+    def transaction_pool():
+        """
+
+        """
+
+
+    def new_block_pool():
+        """
+
+        """
+
+    def voter_pool():
+        """
+
+        """
+
+    def tx_pick():
+        """
+
+        """
+
+    def voter_pick():
+        """
+        get all legal 
+
+        """
