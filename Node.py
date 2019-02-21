e557859b0b2e7ea8ccba0834d77212d5156c843c
Chuan He <hecbuaa@gmail.com>
2019-02-20 18:57:34 -0800

    mining method implementation

--- Node.py
+++ Node.py
@@ -1,7 +1,9 @@
 import os
 import datetime
-import Transactions
-import Blockchain_Util
+import time
+#from Transaction import *
+from Block import block
+from Blockchain_Util import *
 import hashlib
 
 
@@ -10,13 +12,41 @@ class node:
     address = ''
     def __init__(self):
         self.blockchain = []
-    def mining(transactions):
-        return
+
+    def generate_key():
+        return 
+    def mining(self, transactions):
+        difficulty = utils.get_difficulty()
+        zerostr = ''
+        zerostr = zerostr.rjust(difficulty, '0')
+        nonce = 0
+        print zerostr
+        print difficulty
+        time_stamp = time.time()
+        prev_hash = self.blockchain[-1].calculate_hash()
+        print(prev_hash)
+        newblock = block(difficulty, time_stamp, transactions, prev_hash, nonce)
+        hash_val = newblock.calculate_hash()
+        bin_hash_val = ( bin(int(hash_val, 16))[2:] ).zfill(256)
+        print(bin_hash_val[:difficulty])
+        while bin_hash_val[:difficulty] != zerostr:
+            nonce += 1
+            newblock = block(difficulty, time_stamp, transactions, prev_hash, nonce)
+            hash_val = newblock.calculate_hash()
+            bin_hash_val = ( bin(int(hash_val, 16))[2:] ).zfill(256)
+        print(bin_hash_val)
+        return newblock
 
 
         
     def transfer(to_address, amount):
         trans = transaction(self.address, to_address, amount)
+        trans.sign(self.key)
         return trans
-    def signTransactions():
+    
+    def gossipl():
+        return
+    
+    def get_blance():
+        #traverse block get blance
         return
