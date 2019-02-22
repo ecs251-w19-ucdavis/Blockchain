6c45a2ca0ba8261671308962075f6ee56f3a4e3c
Yue Zhang <zhangops@gmail.com>
2019-02-21 22:16:21 -0800

    pick voter function

--- Blockchain_Platform.py
+++ Blockchain_Platform.py
@@ -2,6 +2,9 @@ from Block import block
 import time
 import datetime
 import random
+import json
+from scipy import stats
+import numpy as np
 
 class blockchain_platform:
     def __int__(self, difficulty, reward):
@@ -23,7 +26,10 @@ class blockchain_platform:
     def update_mining_difficulty(self, new_difficulty):
         self.mining_difficulty = new_difficulty
 
-    def get_transaction(self):
+    def add_transaction(self, transaction):
+        self.transaction_pool.append(transaction)
+
+    def pick_transactions(self):
         tx_inds = []
         while len(tx_inds) < 10:
             tx_ind = random.randint(0,10)
@@ -33,23 +39,40 @@ class blockchain_platform:
         for tx_ind in tx_inds:
             tx_list.append(self.transaction_pool[tx_ind])
         return tx_list
+
+
+    def add_voters(self, voter_info):
+        if len(self.voter_pool) >= 100:
+            return False
+        self.voter_pool.append(voter_info)
+
+    def pick_voters(self):
         """
 
         """
+        # lock voter_pool before we pick voters
+        voter_str = json.dumps(self.voter_pool)
+        voter_json = json.loads(voter_str)
+        total = 0
+        for voter in voter_json:
+            total += voter_json[voter]
+        voter_prob = []
+        for voter in voter_json:
+            voter_prob.append(voter_json[voter]/total)
+        index = np.arange(len(self.voter_pool))
+        voter_cdd_index = stats.rv_discrete(name = 'voters_cdd' ,values = (index,voter_prob))
+        voters_cdd = self.voter_pool[voter_cdd_index]
+        return voters_cdd
 
 
-    def new_block_pool():
-        """
+    def assign_nbrs(self):
+        return
 
-        """
+    def generate_key(self):
+        return
 
-    def voter_pool():
-        """
 
-        """
 
-    def voter_pick():
-        """
-        get all legal 
 
-        """
+
+
